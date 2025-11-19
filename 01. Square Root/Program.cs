59165cc3521b8cefa97a855281198f7d868e6aa2
Boyan Venkov <boyanvenkov@yahoo.com>
2025-11-19 16:55:36 +0200

    Add project files.

---
+++ 01. Square Root/Program.cs
@@ -0,0 +1,21 @@
+﻿try
+{
+    int n = int.Parse(Console.ReadLine());
+
+    if (n < 0)
+    {
+        Console.WriteLine("Invalid number.");
+    }
+    else
+    {
+        Console.WriteLine(Math.Sqrt(n));
+    }
+}
+catch
+{
+    Console.WriteLine("Invalid number.");
+}
+finally
+{
+    Console.WriteLine("Goodbye.");
+}
