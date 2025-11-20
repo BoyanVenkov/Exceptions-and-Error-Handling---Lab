6a55da2bc5260d5de157e295231e94a600baad07
Boyan Venkov <boyanvenkov@yahoo.com>
2025-11-20 19:05:35 +0200

    Added a New Project

--- 02. Enter Numbers/Program.cs
+++ 02. Enter Numbers/Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿using System;
+
+int start = 1;
+int end = 100;
+
+ReadNumber(start, end);
+
+static void ReadNumber(int start, int end)
+{
+    List<int> output = new List<int>();
+    int count = 0;
+    int currentNum = start;
+    while (count < 10)
+    {
+        string input = Console.ReadLine();
+
+        try
+        {
+            int num = int.Parse(input);
+
+            if (num <= start || num >= end)
+            {
+                Console.WriteLine($"Your number is not in range {currentNum} - 100!");
+                continue;
+            }
+
+            output.Add(num);
+            start = num;
+            count++;
+            currentNum = num;
+        }
+        catch
+        {
+            Console.WriteLine("Invalid Number!");
+        }
+    }
+
+    Console.WriteLine(string.Join(", ", output));
+}
